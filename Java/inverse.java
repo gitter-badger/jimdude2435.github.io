11986a6aa5afcbaf80194d32038a3b2970b70912
Jimdude2435 <toxicbananaparty@gmail.com>
2015-07-01 20:55:27 -0400

    Memory Usage, Streamlining, and Consistency
    Made variables use less memory by making one global set. Streamlined
    'square' set by make it consistent with the other three sets.

--- Java/inverse.java
+++ Java/inverse.java
@@ -8,6 +8,9 @@ public class Inverses
     {
         Scanner input = new Scanner(System.in);
 
+        int coefficient, inside, outside;
+        int newinside, newoutside, newcoeff;
+
         System.out.println("Types of equations: sqrt cubert square cubed");
         System.out.print("Enter equation: ");
         String eqtype = input.nextLine();
@@ -17,29 +20,22 @@ public class Inverses
             System.out.println("Parabola selected.");
 
             System.out.print("Enter coefficient: ");
-            int coefficient = input.nextInt();
+            coefficient = input.nextInt();
 
             System.out.print("Enter number inside the parentheses: ");
-            int inside = input.nextInt();
+            inside = input.nextInt();
 
             System.out.print("Enter number outside the parentheses: ");
-            int outside = input.nextInt();
+            outside = input.nextInt();
 
-            System.out.println("Your equation should look like this: ");
-            
-            if (inside < 0)
+            System.out.print("Your equation should look like this: ");
+
+            System.out.print("y = " + coefficient + "(x");
+
+            if (inside <0)
             {
-                int newinside = inside * (-1);
-                System.out.print("y = " + coefficient + "(x-" + newinside + ")²");
-                if (outside < 0)
-                {
-                    int newoutside = outside * (-1);
-                    System.out.println(" - " + newoutside);
-                }
-                else
-                {
-                    System.out.println(" + " + outside);
-                }
+                newinside = inside *(-1);
+                System.out.print("-" + newinside + ")²");
             }
             else
             {
@@ -49,7 +45,6 @@ public class Inverses
                     int newoutside = outside * (-1);
                     System.out.println(" - " + newoutside);
                 }
-                else
                 {
                     System.out.println(" + " + outside);
                 }
@@ -62,241 +57,230 @@ public class Inverses
             }
             else
             {
-                int newcoeff = coefficient * (-1);
+                newcoeff = coefficient * (-1);
                 System.out.print("x = " + newcoeff + "√y");
             }
             if (outside < 0)
             {
-                int newoutside = outside * (-1);
+                newoutside = outside * (-1);
                 System.out.print("+" + newoutside);
-                
-                if (inside < 0)
-                {
-                    int newinside = inside * (-1);
-                    System.out.print(" + " + newinside);
-                }
-                else
-                {
-                    System.out.print(" - " + inside);
-                }
             }
             else
             {
                 System.out.print("-" + outside);
-                
-                if (inside < 0)
-                {
-                    int newinside = inside * (-1);
-                    System.out.print(" + " + newinside);
-                }
-                else
-                {
-                    System.out.print(" - " + inside);
-                }
+            }
+            if (inside < 0)
+            {
+                newinside = inside * (-1);
+                System.out.print(" + " + newinside);
+            }
+            else
+            {
+                System.out.print(" - " + inside);
             }
         }
-        
+
         if (eqtype.equals("sqrt"))
         {
             System.out.println("Square root selected.");
-            
+
             System.out.print("Enter coefficient: ");
-            int coefficient = input.nextInt();
-            
+            coefficient = input.nextInt();
+
             System.out.print("\nEnter number inside radical: ");
-            int inside = input.nextInt();
-            
+            inside = input.nextInt();
+
             System.out.print("\nEnter number outside radical: ");
-            int outside = input.nextInt();
-            
+            outside = input.nextInt();
+
             System.out.print("\nYour equation should look like this: ");
-            
+
             System.out.print("y = " + coefficient + "√x");
-            
+
             if (inside < 0)
             {
-                int newinside = inside * (-1);
+                newinside = inside * (-1);
                 System.out.print("-" + newinside);
             }
             else
             {
                 System.out.print("+" + inside);
             }
-            
+
             if (outside < 0)
             {
-                int newoutside = outside * (-1);
+                newoutside = outside * (-1);
                 System.out.print(" - " + newoutside);
             }
             else
             {
                 System.out.print(" + " + outside);
             }
-            
+
             System.out.println("\nIf so, your inverse looks like this: ");
-            
+
             if (coefficient < 0)
             {
-                int newcoeff = coefficient * (-1);
-                System.out.print("x = " + newcoeff + "√y");
-            }
-            else
-            {
-                System.out.print("x = -" + coefficient + "√y");
-            }
-            
-            if (outside < 0)
-            {
-                int newoutside = outside * (-1);
-                System.out.print("+" + newoutside);
-            }
-            else
-            {
-                System.out.print("-" + outside);
-            }
-            
-            if (inside < 0)
-            {
-                int newinside = inside * (-1);
-                System.out.print(" + " + newinside);
-            }
-            else
-            {
-                System.out.print(" - " + inside);
-            }
-        }
-        
-        if (eqtype.equals("cubed"))
-        {
-            System.out.println ("Cubed equation selected.");
-            
-            System.out.print("Enter coefficient: ");
-            int coefficient = input.nextInt();
-            
-            System.out.print("\nEnter number inside parentheses: ");
-            int inside = input.nextInt();
-            
-            System.out.print("\nEnter number outside parenthese: ");
-            int outside = input.nextInt();
-            
-            System.out.print("\nYour equation should look like this: ");
-            
-            System.out.print("y = " + coefficient + "(x");
-            
-            if (inside < 0)
-            {
-                int newinside = inside * (-1);
-                System.out.print("-" + newinside + ")³");
-            }
-            else
-            {
-                System.out.print("+" + inside + ")³");
-            }
-            
-            if (outside < 0)
-            {
-                int newoutside = outside * (-1);
-                System.out.print(" - " + newoutside);
-            }
-            else
-            {
-                System.out.print(" + " + outside);
-            }
-            
-            System.out.print("\nIf so, your inverse looks like this: ");
-            
-            if (coefficient < 0)
-            {
-                int newcoeff = coefficient * (-1);
-                System.out.print("x = " + newcoeff + "³√y");
-            }
-            else
-            {
-                System.out.print("x = -" + coefficient + "³√y");
-            }
-            
-            if (outside < 0)
-            {
-                int newoutside = outside * (-1);
-                System.out.print("+" + newoutside);
-            }
-            else
-            {
-                System.out.print("-" + outside);
-            }
-            
-            if (inside < 0)
-            {
-                int newinside = inside * (-1);
-                System.out.print(" + " + newinside);
-            }
-            else
-            {
-                System.out.print(" - " + inside);
-            }
-        }
-        
-        if (eqtype.equals("cubert"))
-        {
-            System.out.println("Cube root selected.");
-            
-            System.out.print("Enter coefficient: ");
-            int coefficient = input.nextInt();
-            
-            System.out.print("\nEnter number inside radical: ");
-            int inside = input.nextInt();
-            
-            System.out.print("\nEnter number outside radical: ");
-            int outside = input.nextInt();
-            
-            System.out.print("Your equation should look like this: ");
-            System.out.print("y = " + coefficient + "³√x");
-            
-            if (inside < 0)
-            {
-                int newinside = inside * (-1);
-                System.out.print("-" + newinside);
-            }
-            else
-            {
-                System.out.print("+" + inside);
-            }
-            
-            if (outside < 0)
-            {
-                int newoutside = outside * (-1);
-                System.out.print(" - " + newoutside);
-            }
-            else
-            {
-                System.out.print(" + " + outside);
-            }
-            
-            System.out.print("\nIf so, your inverse looks like this: ");
-            
-            if (coefficient < 0)
-            {
-                int newcoeff = coefficient * (-1);
+                newcoeff = coefficient * (-1);
                 System.out.print("x = " + newcoeff + "(y");
             }
             else
             {
                 System.out.print("x = -" + coefficient + "(y");
             }
-            
+
             if (outside < 0)
             {
-                int newoutside = outside * (-1);
+                newoutside = outside * (-1);
+                System.out.print("+" + newoutside + ")²");
+            }
+            else
+            {
+                System.out.print("-" + outside + ")²");
+            }
+
+            if (inside < 0)
+            {
+                newinside = inside * (-1);
+                System.out.print(" + " + newinside);
+            }
+            else
+            {
+                System.out.print(" - " + inside);
+            }
+        }
+
+        if (eqtype.equals("cubed"))
+        {
+            System.out.println ("Cubed equation selected.");
+
+            System.out.print("Enter coefficient: ");
+            coefficient = input.nextInt();
+
+            System.out.print("\nEnter number inside parentheses: ");
+            inside = input.nextInt();
+
+            System.out.print("\nEnter number outside parenthese: ");
+            outside = input.nextInt();
+
+            System.out.print("\nYour equation should look like this: ");
+
+            System.out.print("y = " + coefficient + "(x");
+
+            if (inside < 0)
+            {
+                newinside = inside * (-1);
+                System.out.print("-" + newinside + ")³");
+            }
+            else
+            {
+                System.out.print("+" + inside + ")³");
+            }
+
+            if (outside < 0)
+            {
+                newoutside = outside * (-1);
+                System.out.print(" - " + newoutside);
+            }
+            else
+            {
+                System.out.print(" + " + outside);
+            }
+
+            System.out.print("\nIf so, your inverse looks like this: ");
+
+            if (coefficient < 0)
+            {
+                newcoeff = coefficient * (-1);
+                System.out.print("x = " + newcoeff + "³√y");
+            }
+            else
+            {
+                System.out.print("x = -" + coefficient + "³√y");
+            }
+
+            if (outside < 0)
+            {
+                newoutside = outside * (-1);
+                System.out.print("+" + newoutside);
+            }
+            else
+            {
+                System.out.print("-" + outside);
+            }
+
+            if (inside < 0)
+            {
+                newinside = inside * (-1);
+                System.out.print(" + " + newinside);
+            }
+            else
+            {
+                System.out.print(" - " + inside);
+            }
+        }
+
+        if (eqtype.equals("cubert"))
+        {
+            System.out.println("Cube root selected.");
+
+            System.out.print("Enter coefficient: ");
+            coefficient = input.nextInt();
+
+            System.out.print("\nEnter number inside radical: ");
+            inside = input.nextInt();
+
+            System.out.print("\nEnter number outside radical: ");
+            outside = input.nextInt();
+
+            System.out.print("Your equation should look like this: ");
+            System.out.print("y = " + coefficient + "³√x");
+
+            if (inside < 0)
+            {
+                newinside = inside * (-1);
+                System.out.print("-" + newinside);
+            }
+            else
+            {
+                System.out.print("+" + inside);
+            }
+
+            if (outside < 0)
+            {
+                newoutside = outside * (-1);
+                System.out.print(" - " + newoutside);
+            }
+            else
+            {
+                System.out.print(" + " + outside);
+            }
+
+            System.out.print("\nIf so, your inverse looks like this: ");
+
+            if (coefficient < 0)
+            {
+                newcoeff = coefficient * (-1);
+                System.out.print("x = " + newcoeff + "(y");
+            }
+            else
+            {
+                System.out.print("x = -" + coefficient + "(y");
+            }
+
+            if (outside < 0)
+            {
+                newoutside = outside * (-1);
                 System.out.print("+" + newoutside + ")³");
             }
             else
             {
                 System.out.print("-" + outside + ")³");
             }
-            
+
             if (inside < 0)
             {
-                int newinside = inside * (-1);
+                newinside = inside * (-1);
                 System.out.print(" + " + newinside);
             }
             else
